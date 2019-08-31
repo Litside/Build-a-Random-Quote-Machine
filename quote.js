1415d7275064f62f64a001ec0581d8600956950f
Litside <297931849@qq.com>
2019-09-01 00:38:03 +0800

    add fresh

--- quote.js
+++ quote.js
@@ -1,3 +1,5 @@
+window.onload=linkupdata;
+
 function rc(){
 	return parseInt(Math.random()*256);
 }
@@ -5,7 +7,21 @@ $(document).ready(function() {
 
 				
 	$("#clickbutton").on("click", function(){
-		$.getJSON('https://v1.hitokoto.cn/', function(json) {
+			linkupdata();
+		
+		});
+});
+
+function twitter(){
+window.location.href='https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=%22The%20optimist%20proclaims%20that%20we%20live%20in%20the%20best%20of%20all%20possible%20worlds%2C%20and%20the%20pessimist%20fears%20this%20is%20true.%22%20James%20Branch%20Cabell';
+}
+function lumber(){
+	window.location.href='https://www.tumblr.com/login?redirect_to=https%3A%2F%2Fwww.tumblr.com%2Fwidgets%2Fshare%2Ftool%3Fposttype%3Dquote%26tags%3Dquotes%252Cfreecodecamp%26caption%3DJames%2BBranch%2BCabell%26content%3DThe%2Boptimist%2Bproclaims%2Bthat%2Bwe%2Blive%2Bin%2Bthe%2Bbest%2Bof%2Ball%2Bpossible%2Bworlds%252C%2Band%2Bthe%2Bpessimist%2Bfears%2Bthis%2Bis%2Btrue.%26canonicalUrl%3Dhttps%253A%252F%252Fwww.tumblr.com%252Fbuttons%26shareSource%3Dtumblr_share_button'
+}
+
+function linkupdata(){
+
+			$.getJSON('https://v1.hitokoto.cn/', function(json) {
 			
 				/*optional stuff to do after success */
 				var color1=rc();
@@ -21,15 +37,5 @@ $(document).ready(function() {
 				$(".sharetumblrbutton").css("background-color","rgb("+sumcolor+")");
 				$(".author").html("- "+json.creator);
 
-
-
-		});	
 		});
-});
-
-function twitter(){
-window.location.href='https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=%22The%20optimist%20proclaims%20that%20we%20live%20in%20the%20best%20of%20all%20possible%20worlds%2C%20and%20the%20pessimist%20fears%20this%20is%20true.%22%20James%20Branch%20Cabell';
-}
-function lumber(){
-	window.location.href='https://www.tumblr.com/login?redirect_to=https%3A%2F%2Fwww.tumblr.com%2Fwidgets%2Fshare%2Ftool%3Fposttype%3Dquote%26tags%3Dquotes%252Cfreecodecamp%26caption%3DJames%2BBranch%2BCabell%26content%3DThe%2Boptimist%2Bproclaims%2Bthat%2Bwe%2Blive%2Bin%2Bthe%2Bbest%2Bof%2Ball%2Bpossible%2Bworlds%252C%2Band%2Bthe%2Bpessimist%2Bfears%2Bthis%2Bis%2Btrue.%26canonicalUrl%3Dhttps%253A%252F%252Fwww.tumblr.com%252Fbuttons%26shareSource%3Dtumblr_share_button'
 }
